2061bbc8db142e44c83e576ce42c75c0be21ff79
yechan <yyechan@naver.com>
2021-10-20 16:03:02 +0900

    2021-10-20
    dict path fixed

--- dictionary.py
+++ dictionary.py
@@ -5,7 +5,7 @@ class Dictionary:
     data = None
 
     def load_dict(self, file_name):
-        with open('data/' + file_name, 'rb') as f:
+        with open('data/dict/' + file_name, 'rb') as f:
             data = utils.pickle.load(f)
 
     def get_data(self):
